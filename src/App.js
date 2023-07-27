0f00f5899965112b852c57201b91a989143aba78
emangomaa <emangomaa149@gmail.com>
2023-07-27 05:26:23 +0300

    app routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import "./App.css";
+import { useState } from "react";
+import Form from "./components/Form";
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
 
 function App() {
-  return <div className="App"></div>;
+  let [items, setItems] = useState([]);
+
+  function addItem(item) {
+    setItems((items) => [...items, item]);
+  }
+  function deleteItem(id) {
+    setItems(items.filter((item) => item.id !== id));
+  }
+
+  function clearList() {
+    let confirm = window.confirm(
+      "Are You Sure To Delete All Items Of The List ?"
+    );
+    if (confirm) {
+      setItems([]);
+    }
+  }
+
+  function toggleItem(id) {
+    setItems(
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form addItem={addItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={deleteItem}
+        onClearList={clearList}
+        onToggleItem={toggleItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
 }
 
 export default App;
