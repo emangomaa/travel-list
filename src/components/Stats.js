3f43fdd6e6f30b503b7221ef688a0db3e1a2e169
emangomaa <emangomaa149@gmail.com>
2023-07-27 05:24:53 +0300

    footer component

---
+++ src/components/Stats.js
@@ -0,0 +1,23 @@
+const Stats = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <em className="stats">Start adding some items to your packing list 🚀</em>
+    );
+  }
+  let listCount = items.length;
+  let packedCount = items.filter((item) => item.packed).length;
+  let percentage = Math.round((packedCount / listCount) * 100);
+  return (
+    <footer className="stats">
+      {percentage === 100 ? (
+        <em>You got every thing! Ready to go ✈️</em>
+      ) : (
+        <p>
+          `💼 You have {listCount} items on your list, and you Already packed{" "}
+          {packedCount} ({percentage}%)`{" "}
+        </p>
+      )}
+    </footer>
+  );
+};
+export default Stats;
